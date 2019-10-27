d6a8a54c7e0e1332a478c9ecd7c398e065d0f6b5
Mariah Lightfoot <mariahlightfoot@Mariahs-MacBook-Pro.local>
2019-10-27 17:20:32 -0500

    wrote down thoughts

---
+++ scripts.js
@@ -0,0 +1,4 @@
+//array that houses all of the image files
+//function will randomly select an image to display
+//when that image is selected, it will be placed into a new array
+//conditional will say if in the new array, do not display again
